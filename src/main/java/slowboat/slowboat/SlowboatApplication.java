f450d72eed08f3358a2becf0a4ac1f687bd945a4
hhss2259 <hhss2259@naver.com>
2023-05-27 21:44:53 +0900

    first commit

---
+++ src/main/java/slowboat/slowboat/SlowboatApplication.java
@@ -0,0 +1,13 @@
+package slowboat.slowboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowboatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowboatApplication.class, args);
+	}
+
+}
